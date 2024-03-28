fa4809e08bfbacd723e749a8f1e17c0d8884005e
koe <ukoe@protonmail.com>
2024-03-28 15:00:42 -0500

    remove game region example as not planned

--- examples/game_regions.rs
+++
@@ -1,6 +0,0 @@
-//! Demonstrates a game with multiple 'regions' that are each separate game worlds.
-
-//Main menu -> start game (displays region #) -> move to new region (displays region #) -> move back to starting
-// region.
-
-fn main() {}
