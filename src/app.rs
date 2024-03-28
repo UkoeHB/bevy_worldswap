b38bd28155016f4f5335a50504312c3bb0343c0d
koe <ukoe@protonmail.com>
2024-03-28 15:03:19 -0500

    doc fixes

--- src/app.rs
+++ src/app.rs
@@ -11,15 +11,15 @@ use crate::*;
 
 /// Command that can be sent with [`SwapCommandSender`] to control which world is running.
 ///
-/// Swap commands provide a simple 1-layer 'fork-join' pattern. Use [`Self::Fork`] in the initial world to put it
-/// in the background and run another world in the foreground. Use [`Self::Pass`] to drop the foreground world and
-/// run another world in the foreground. Use [`Self::Join`] to drop the foreground world and put the background
+/// Swap commands provide a simple 1-layer 'fork-join' pattern. Use [`Fork`](SwapCommand::Fork) in the initial world to put it
+/// in the background and run another world in the foreground. Use [`Pass`](SwapCommand::Pass) to drop the foreground world and
+/// run another world in the foreground. Use [`Join`](SwapCommand::Join) to drop the foreground world and put the background
 /// world in the foreground.
 ///
-/// Both the foreground and background worlds can send [`Self::Pass`], [`Self::Swap`], and [`Self::Join`] commands.
-/// Only foreground worlds can send [`Self::Fork`], and only if there is no background world.
+/// Both the foreground and background worlds can send [`Pass`](SwapCommand::Pass), [`Swap`](SwapCommand::Swap), and [`Join`](SwapCommand::Join) commands.
+/// Only foreground worlds can send [`Fork`](SwapCommand::Fork), and only if there is no background world.
 ///
-/// Note that when a world is dropped due to [`Self::Pass`] or [`Self::Join`], an [`AppExit`] event will not be
+/// Note that when a world is dropped due to [`Pass`](SwapCommand::Pass) or [`Join`](SwapCommand::Join), an `AppExit` event will not be
 /// sent to that world unless the world generated the event itself.
 pub enum SwapCommand
 {
@@ -39,7 +39,7 @@ pub enum SwapCommand
     Swap,
     /// Swap in the background world and drop the current world.
     ///
-    /// Note that if the background world sent [`AppExit`] at any point in the past, then as soon as it enters the
+    /// Note that if the background world sent `AppExit` at any point in the past, then as soon as it enters the
     /// foreground the app will shut down.
     ///
     /// # Panics
@@ -79,7 +79,7 @@ pub(crate) struct SwapCommandReceiver(pub(crate) crossbeam::channel::Receiver<Sw
 
 /// Resource that records the world-swap status of a world.
 ///
-/// This is controlled internally by [`WorldSwapSubApp`].
+/// This is controlled by the `bevy_worldswap` backend.
 #[derive(Resource, Copy, Clone, Eq, PartialEq)]
 pub enum WorldSwapStatus
 {
